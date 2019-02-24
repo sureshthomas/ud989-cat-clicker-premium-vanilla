4f6044c307fe1cffeafb77455feb1e30366682ad
suresh.thomas@yahoo.com <Leena@123>
2019-02-24 22:39:53 +0000

    Changes by Suresh

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 
 /* ======= Model ======= */
 
-var model = {
+let model = {
     currentCat: null,
     cats: [
         {
@@ -40,45 +40,109 @@ var model = {
 
 /* ======= Octopus ======= */
 
-var octopus = {
+let octopus = {
 
-    init: function() {
+    init: function () {
         // set our current cat to the first one in the list
         model.currentCat = model.cats[0];
 
         // tell our views to initialize
+        adminFormView.init();
+        adminButtonView.init();
         catListView.init();
         catView.init();
+
     },
 
-    getCurrentCat: function() {
+    getCurrentCat: function () {
         return model.currentCat;
     },
 
-    getCats: function() {
+    getCats: function () {
         return model.cats;
     },
 
     // set the currently-selected cat to the object passed in
-    setCurrentCat: function(cat) {
+    setCurrentCat: function (cat) {
         model.currentCat = cat;
     },
 
     // increments the counter for the currently-selected cat
-    incrementCounter: function() {
+    incrementCounter: function () {
         model.currentCat.clickCount++;
         catView.render();
+
     }
 };
 
 
 /* ======= View ======= */
 
-var catView = {
+function  submit() {
+    let currentCat = octopus.getCurrentCat();
+    currentCat.name = document.getElementById('cat-admin-form-name').value;
+    currentCat.imgSrc = document.getElementById('cat-admin-form-name-url').value;
+    currentCat.clickCount = document.getElementById('cat-admin-form-clk').value;
+    octopus.setCurrentCat(currentCat);
+}
+
+let  adminFormView = {
+
+
+    init:function () {
+        this.adminForm = document.getElementById('admin-form');
+        console.log(`init admin form ${this.adminForm}`);
+        this.visible = true;
+        this.render();
+        this.submitButton = document.getElementById('form-submit');
+
+        this.submitButton.addEventListener('click',function () {
+            submit();
+        });
+
+
+    },
+    setVisible: function(boolFlag){
+        this.visible = boolFlag;
+
+    },
+    getVisible: function(){
+        return this.visible;
+    },
+    render:function () {
+        console.log(`admin form ${this.adminForm}`);
+        if (this.visible ) {
+            let currentCat = octopus.getCurrentCat();
+
+            document.getElementById('cat-admin-form-name').value = currentCat.name;
+            document.getElementById('cat-admin-form-name-url').value = currentCat.imgSrc;
+            document.getElementById('cat-admin-form-clk').value = currentCat.clickCount;
+            this.adminForm.style.display = 'block';
+        }
+        else{
+           this.adminForm.style.display = 'none';
+        }
+    }
+
+};
+
+let adminButtonView = {
+    init:function () {
+        this.admButtonId = document.getElementById('admin-btn');
+        this.admButtonId.addEventListener('click', function(){
+
+            adminFormView.setVisible(!adminFormView.getVisible());
+            adminFormView.render();
+        })
+
+    }
+};
+
+let catView = {
 
     init: function() {
         // store pointers to our DOM elements for easy access later
-        this.catElem = document.getElementById('cat');
+        //this.catElem = document.getElementById('cat');
         this.catNameElem = document.getElementById('cat-name');
         this.catImageElem = document.getElementById('cat-img');
         this.countElem = document.getElementById('cat-count');
@@ -90,18 +154,21 @@ var catView = {
 
         // render this view (update the DOM elements with the right values)
         this.render();
+        //adminFormView.render();
     },
 
     render: function() {
         // update the DOM elements with values from the current cat
-        var currentCat = octopus.getCurrentCat();
+        let currentCat = octopus.getCurrentCat();
         this.countElem.textContent = currentCat.clickCount;
         this.catNameElem.textContent = currentCat.name;
         this.catImageElem.src = currentCat.imgSrc;
+        this.catImageElem.classList.add('image-link');
+         adminFormView.render();
     }
 };
 
-var catListView = {
+let catListView = {
 
     init: function() {
         // store the DOM element for easy access later
@@ -112,9 +179,9 @@ var catListView = {
     },
 
     render: function() {
-        var cat, elem, i;
+        let  cat, elem, i;
         // get the cats we'll be rendering from the octopus
-        var cats = octopus.getCats();
+        let  cats = octopus.getCats();
 
         // empty the cat list
         this.catListElem.innerHTML = '';
@@ -127,6 +194,7 @@ var catListView = {
             // make a new cat list item and set its text
             elem = document.createElement('li');
             elem.textContent = cat.name;
+            elem.classList.add('list-item-link');
 
             // on click, setCurrentCat and render the catView
             // (this uses our closure-in-a-loop trick to connect the value
